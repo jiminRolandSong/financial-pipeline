4ab19328d5d02cbefeb9b09b361d1f02bd665eb2
Jimin Song <aangava1117@gmail.com>
2026-05-29 17:44:01 -0400

    Replace print statements with logging for improved traceability in lambda_handler

--- lambda/transform_load_lambda/lambda_function.py
+++ lambda/transform_load_lambda/lambda_function.py
@@ -1,15 +1,19 @@
 import sys
 import os
+import logging
+
 sys.path.append('/var/task')
 
 from transform import run as transform_run
+
+logger = logging.getLogger(__name__)
 
 def lambda_handler(event, context):
     for record in event.get('Records', []):
         bucket = record['s3']['bucket']['name']
         key = record['s3']['object']['key']
-        print(f'Triggered by: s3://{bucket}/{key}')
-    
+        logger.info('Triggered by: s3://%s/%s', bucket, key)
+
     transform_run()
     return {
         'statusCode': 200,
